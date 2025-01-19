a63197f80f488df217059ef671dd120f75765b80
Alexandr Savca <alexandr.savca89@gmail.com>
2025-01-19 06:00:25 +0200

    mksslcert: clean up the script and man page, 0.1.1 -> 0.1.2

--- mksslcert/mksslcert.sh
+++ mksslcert/mksslcert.sh
@@ -16,18 +16,18 @@ main() {
 		print_help 1>&2
 		exit 1
 	fi
-	
+
 	KEY=$1
 	CRT=$2
 	FQDN=$(hostname -f) || FQDN=localhost
-	if [ ! -z "$3" ]; then
+	if [ -n "$3" ]; then
 		FQDN="$3"
 	fi
 	INFO=".\n.\n.\n.\n.\n$FQDN\nroot@$FQDN"
 	OPTS="req -new -nodes -x509 -days 365 -newkey rsa:2048"
-	
+
 	printf "$INFO\n" | openssl $OPTS -out $CRT -keyout $KEY 2> /dev/null
-	
+
 	if [ $? -ne 0 ]; then
 		echo "Error: creating of certificate failed" 1>&2
 		exit 1
